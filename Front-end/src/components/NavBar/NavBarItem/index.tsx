fd112c31365f2527b598aa6d24b83e2e6eb00c68
lancerace <lancerace144@gmail.com>
2021-03-20 22:24:45 +0800

    add hover effect

--- Front-end/src/components/NavBar/NavBarItem/index.tsx
+++ Front-end/src/components/NavBar/NavBarItem/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
 export default function NavBarItem({ children, to }) {
+
+
+
   return (
     <li>
       <Link to={to}>{children}</Link>
